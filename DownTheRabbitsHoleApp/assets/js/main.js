f925f1d70fc0026d22c8537a62ca1e6446f4ff1a
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-06-02 18:55:40 -0400

    Update main.js

--- DownTheRabbitsHoleApp/assets/js/main.js
+++ DownTheRabbitsHoleApp/assets/js/main.js
@@ -62,15 +62,22 @@ toggleThemeBtn.addEventListener('click', () =>{
 });
 
 // Swiper
-const swiper = new Swiper('.swiper',{
+const swiper = new Swiper('swiper',{
   slidesPerView: 1,
   spaceBetween: 20,
-  navgation: {
+  navigation: {
     nextEl: '.swiper-btn-next',
     prevEl: '.swiper-btn-prev'
   },
   pagination: {
-    el: '.swiper-pagination'
+    el: ',swiper-pagination'
   },
-  
+  breakpoints: {
+    700: {
+      slidesPerView: 2
+    },
+    1200: {
+      slidesPerView: 3
+    }
+  }
 });
