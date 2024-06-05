2c3a975bf5025631a2b7a3b8b9213974a4b2dda4
Kaide Ryder <157544440+BraveLilFox@users.noreply.github.com>
2024-06-05 14:08:19 -0400

    Update main.js

--- DownTheRabbitsHoleApp/assets/js/main.js
+++ DownTheRabbitsHoleApp/assets/js/main.js
@@ -63,8 +63,8 @@ toggleThemeBtn.addEventListener('click', () =>{
 
 // Swiper
 const swiper = new Swiper('swiper', {
-  slidesPerView = 1,
-  spaceBetween = 20,
+  slidesPerVie: 1,
+  spaceBetween: 20,
   navigation:{
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
